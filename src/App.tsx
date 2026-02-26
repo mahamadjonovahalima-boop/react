53fb437d9dd654d84cd616efed316b1b593e74bc
halima <mahamadjonovahalima@gmail.com>
2026-02-26 10:09:54 +0600

    pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
 import { useState } from 'react';
-import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name,setName,] = useState('pikachu');
+  const [pokemon, setPokemon] = useState(null);
+
+  const handleSearchPokemon = async() =>{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+
+     setPokemon ({
+      name: data.name,
+      image: data.sprites.front_default,
+      height: data.height,
+      weight: data.weight,
+      types: data.types.map(t => t.type.name),
+    });
+  
+  };
+
+
 
   return (
     <>
-      <h1>Beknur</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count + 1)}>count is {count}</button>
+      <>
+      <h1>Pokémon Viewer</h1>
+      <p className='subtitle'>Загрузи любого покемона из интернета</p>
+
+      <div className='controls'>
+        <input id='pokemonInput' type='text' placeholder='Например: pikachu'  onChange={(e) => setName(e.target.value)}/>
+        <button id='loadBtn' onClick={handleSearchPokemon}>
+          Загрузить
+        </button>
+        <button id='randomBtn'>🎲 Random</button>
       </div>
+
+      <p id='status' className='status'></p>
+
+      <div id='card' className='card-container'>
+        {pokemon ? (
+          <div className='card'>
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.image} />
+
+            <p>Рост: {pokemon.height}</p>
+            <p>Вес: {pokemon.weight}</p>
+
+            <div className='types'>
+              <span className='type'>Thunder</span>
+           </div>
+        </div>
+      ) : (
+        'пусто'
+      )}
+    </div>
+  </>
+
+      
     </>
   );
 }
