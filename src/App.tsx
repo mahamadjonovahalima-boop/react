f9a8746efdbce17bf37bce4ecd0c47d0a37bde50
mahamadjonovahalima-boop <mahamadjonovahalima@gmail.com>
2026-02-28 08:08:08 +0300

    add: randmp pokemon load

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,51 @@
 import { useState } from 'react';
 
-
 function App() {
-  const [name,setName,] = useState('pikachu');
+  const [name, setName] = useState('pikachu');
   const [pokemon, setPokemon] = useState(null);
 
-  const handleSearchPokemon = async() =>{
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const loadPokemon = async nameOrId => {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${nameOrId}`,
+    );
     const data = await response.json();
 
-     setPokemon ({
+    setPokemon({
       name: data.name,
       image: data.sprites.front_default,
       height: data.height,
       weight: data.weight,
       types: data.types.map(t => t.type.name),
     });
-  
   };
 
+  const handleSearchPokemon = () => {
+    loadPokemon(name);
+  };
 
+  const handleRandomPokemon = () => {
+    const randomId = Math.floor(Math.random() * 1025) + 1;
+    loadPokemon(randomId);
+  };
 
   return (
     <>
-      <>
       <h1>Pokémon Viewer</h1>
       <p className='subtitle'>Загрузи любого покемона из интернета</p>
 
       <div className='controls'>
-        <input id='pokemonInput' type='text' placeholder='Например: pikachu'  onChange={(e) => setName(e.target.value)}/>
+        <input
+          id='pokemonInput'
+          type='text'
+          placeholder='Например: pikachu'
+          onChange={e => setName(e.target.value)}
+        />
         <button id='loadBtn' onClick={handleSearchPokemon}>
           Загрузить
         </button>
-        <button id='randomBtn'>🎲 Random</button>
+        <button id='randomBtn' onClick={handleRandomPokemon}>
+          🎲 Random
+        </button>
       </div>
 
       <p id='status' className='status'></p>
@@ -47,16 +60,15 @@ function App() {
             <p>Вес: {pokemon.weight}</p>
 
             <div className='types'>
-              <span className='type'>Thunder</span>
-           </div>
-        </div>
-      ) : (
-        'пусто'
-      )}
-    </div>
-  </>
-
-      
+              {pokemon.types.map(type => (
+                <span className='type'>{type}</span>
+              ))}
+            </div>
+          </div>
+        ) : (
+          'пусто'
+        )}
+      </div>
     </>
   );
 }
